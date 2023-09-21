e5ea5dcb516e4785577520ac4f086a396f3ed379
AKASH KUMAR PRAJAPATI <akashkumarprajapati2003@gmail.com>
2023-09-21 15:29:02 +0530

    Project done

--- client/src/components/admin/QueryDetail.js
+++ client/src/components/admin/QueryDetail.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from "react";
 export default function QueryDetail() {
   const [data, setData] = useState(null);
   const [editIndex, setEditIndex] = useState(-1);
-  const [editedName, setEditedName] = useState("");
+  const [editedData, setEditedData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchData();
@@ -12,113 +13,127 @@ export default function QueryDetail() {
   const fetchData = async () => {
     try {
       const response = await fetch("http://localhost:8081/get/all/query");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
       const jsonData = await response.json();
       setData(jsonData);
+      setError(null); // Clear any previous errors
     } catch (error) {
-      console.error("Error fetching data:", error);
+      setError("Error fetching data: " + error.message);
     }
   };
 
   const handleEdit = (index) => {
     setEditIndex(index);
-    setEditedName(data.queryForms[index].name);
+    setEditedData({ ...data.queryForms[index] });
   };
 
   const handleUpdate = async (index) => {
     try {
-      const updatedData = { ...data.queryForms[index], name: editedName };
-      await fetch(`http://localhost:8081/update/query/${data.queryForms[index]._id}`, {
+      const updatedData = { ...data.queryForms[index], ...editedData };
+      const response = await fetch(`http://localhost:8081/update/query/${data.queryForms[index]._id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(updatedData),
       });
-      fetchData();
-      setEditIndex(-1);
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      fetchData(); // Refetch data to update the component state
+      setEditIndex(-1); // Reset edit mode
+      setError(null); // Clear any previous errors
     } catch (error) {
-      console.error("Error updating data:", error);
+      setError("Error updating data: " + error.message);
     }
   };
 
-  const handleDelete = async (index) => {
-    try {
-      await fetch(`http://localhost:8081/delete/query/${data.queryForms[index]._id}`, {
-        method: "DELETE",
-      });
-      fetchData();
-    } catch (error) {
-      console.error("Error deleting data:", error);
-    }
-  };
-
-  // Render the fetched data
+  // Render the fetched data and error messages
   return (
     <div className="h-auto rounded-lg p-10">
       <div className="w-[80vw] rounded-lg flex-wrap">
-        {data ? (
-          <div>
-            <h2 className="text-white flex justify-center text-3xl mb-5 font-semibold">Student Query List</h2>
-            <table className="text-white">
-              <thead className="bg-gray-800">
-                <tr>
-                  <th className="py-2 px-10 text-white">SrNo.</th>
-                  <th className="py-2 text-white">Name</th>
-                  <th className="py-2 text-white">Email</th>
-                  <th className="py-2 text-white">Mobile No</th>
-                  <th className="py-2 text-white">Applied Course</th>
-                  <th className="py-2 text-white">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.queryForms.map((queryForm, index) => (
-                  <tr key={queryForm._id}>
-                    <td className="py-0 px-8 text-white">{index + 1}.</td>
-                    <td className="py-0 text-white">
-                      {editIndex === index ? (
-                        <input
-                          type="text"
-                          value={editedName}
-                          onChange={(e) => setEditedName(e.target.value)}
-                          className="bg-white text-black px-2 py-1 rounded"
-                        />
-                      ) : (
-                        queryForm.name
-                      )}
-                    </td>
-                    <td className="py-0 px-2 text-white">{queryForm.email}</td>
-                    <td className="py-0 px-10 text-white">{queryForm.mobileNo}</td>
-                    <td className="py-0 px-10 text-white">{queryForm.courseToApply}</td>
-                    <td className="py-2 px-2 text-white">
-                      {editIndex === index ? (
-                        <button
-                          className="bg-green-500 text-white px-6 py-2 rounded mx-3"
-                          onClick={() => handleUpdate(index)}
-                        >
-                          Save
-                        </button>
-                      ) : (
-                        <button
-                          className="bg-blue-500 text-white px-6 py-2 rounded mx-3"
-                          onClick={() => handleEdit(index)}
-                        >
-                          Edit
-                        </button>
-                      )}
-                      <button
-                        className="bg-red-500 text-white px-4 py-2 rounded"
-                        onClick={() => handleDelete(index)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+        {error ? (
+          <p className="text-red-500">{error}</p>
         ) : (
-          <p>Loading data...</p>
+          data && (
+            <div>
+              <h2 className="text-white flex justify-center text-3xl mb-5 font-semibold">Student Query List</h2>
+              <table className="text-white">
+                <thead className="bg-gray-800">
+                  <tr>
+                    <th className="py-2 px-10 text-white">SrNo.</th>
+                    <th className="py-2 text-white">Name</th>
+                    <th className="py-2 pl-5 text-white">Email</th>
+                    <th className="py-2 pl-16 text-white">Mobile No</th>
+                    <th className="py-2 pl-28 px-10  text-white">Applied Course</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.queryForms.map((queryForm, index) => (
+                    <tr key={queryForm._id}>
+                      <td className="py-0 px-8 pl-10 text-white">{index + 1}.</td>
+                      <td className="py-0 pl-5 text-white">
+                        {editIndex === index ? (
+                          <input
+                            type="text"
+                            value={editedData.name}
+                            onChange={(e) => setEditedData({ ...editedData, name: e.target.value })}
+                            className="bg-white text-black px-2 py-1 rounded"
+                          />
+                        ) : (
+                          queryForm.name
+                        )}
+                      </td>
+                      <td className="py-0 pl-16 text-white">
+                        {editIndex === index ? (
+                          <input
+                            type="text"
+                            value={editedData.email}
+                            onChange={(e) => setEditedData({ ...editedData, email: e.target.value })}
+                            className="bg-white text-black px-2 py-1 rounded"
+                          />
+                        ) : (
+                          queryForm.email
+                        )}
+                      </td>
+                      <td className="py-2 pl-28 px-10 text-white">
+                        {editIndex === index ? (
+                          <input
+                            type="text"
+                            value={editedData.mobileNo}
+                            onChange={(e) =>
+                              setEditedData({ ...editedData, mobileNo: e.target.value })
+                            }
+                            className="bg-white text-black px-2 py-1 rounded"
+                          />
+                        ) : (
+                          queryForm.mobileNo
+                        )}
+                      </td>
+                      <td className="py-2 pl-28 px-10  text-white">
+                        {editIndex === index ? (
+                          <input
+                            type="text"
+                            value={editedData.courseToApply}
+                            onChange={(e) =>
+                              setEditedData({ ...editedData, courseToApply: e.target.value })
+                            }
+                            className="bg-white text-black px-2 py-1 rounded"
+                          />
+                        ) : (
+                          queryForm.courseToApply
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )
         )}
       </div>
     </div>
